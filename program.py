e03ada4e8a6fd8369dca349c4c160570ef89e902
Natalia Kniat (Nokia) <natalia.kniat@nokia.com>
2023-09-09 18:20:23 +0200

    usuniecie czesci logiki

--- program.py
+++ program.py
@@ -3,14 +3,8 @@
 #  wiek, plec, wyksztalcenie
 #  Program podpowiada w co inwestowac
 
-kwota = int(input('Ile masz wolnych srodków w PLN?'))
-
 #  funkcja przelicznik(int) konwertuje PLN na USD
 kwota_w_USD = przelicznik(kwota)
-
-wiek = input('Ile masz lat' )
-plec = input('Kobieta (K) czy mezczyzna (M)?' )
-wyksztalcenie = input('Jakie masz wykształcenie? (podstawowe (P), srednie (S), wyzsze (W))')
 
 #  funkcja poziom_ryzyka zwraca liczbę od 0 do 10, która okresla poziom ryzyka,
 #  gdzie 0 - brak ryzyka; 10 - ekstremalnie wysokie ryzyko
