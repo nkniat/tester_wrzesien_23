65559c9156f872a13cea80f2cdd992b96b72e436
nkniat <101292925+nkniat@users.noreply.github.com>
2023-09-10 09:03:22 +0200

    import plikow

--- program.py
+++ program.py
@@ -1,3 +1,6 @@
+from funkcja_przelicznik import *
+import funkcja_ryzyko, funkcja_rodzaj_inwestycji
+
 #  Program przyjmuje ilosc pieniedzy w PLN i konweruje na USD
 #  Program ustala akceptowalny poziom ryzyka na podstawie danych:
 #  wiek, plec, wyksztalcenie
@@ -8,7 +11,7 @@ kwota_w_USD = przelicznik(kwota)
 
 #  funkcja poziom_ryzyka zwraca liczbę od 0 do 10, która okresla poziom ryzyka,
 #  gdzie 0 - brak ryzyka; 10 - ekstremalnie wysokie ryzyko
-ryzyko = poziom_ryzyka(wiek, plec, wyksztalcenie)
+ryzyko = funkcja_ryzyko.poziom_ryzyka(wiek, plec, wyksztalcenie)
 
 #  funkcja na podstawie poziomu ryzyka i wysokosci kapitalu sugeruje w co inwestowac
-rodzaj_inwestycji(ryzyko, kwota_w_USD)
+funkcja_rodzaj_inwestycji.rodzaj_inwestycji(ryzyko, kwota_w_USD)
